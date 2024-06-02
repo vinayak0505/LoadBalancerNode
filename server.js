418c45a108823da3100e0c67bfcfcf4f1fe45b4e
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-06-02 17:21:43 +0530

    added leastConnect algo

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const http = require('http');
 const roundRobin = require('./Algo/roundRobin');
+const leastConnections = require('./Algo/leastConnection');
 const serverConfig = require('./config.json').servers;
 
 const servers = serverConfig.map((server) => ({
-    ...server
+    ...server,
+    connections: 0
 }))
 
-const loadBalancingAlgorithm = 'roundRobin';
+const loadBalancingAlgorithm = 'leastConnection';
 
 const server = http.createServer((req, res) => {
     switch (loadBalancingAlgorithm) {
         case 'roundRobin':
             roundRobin(servers, req, res);
             break;
-
+        case 'leastConnection':
+            leastConnections(servers, req, res);
+            break;
         default:
             res.writeHead(500);
             res.end('Invalid load balancing algorithm');
